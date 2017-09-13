87fea0af34b4f31eb135dac816207cb16634b45b
Vitaliy <dockdep@gmail.com>
2017-09-14 00:29:09 +0300

    start

--- frontend/widgets/views/_filter_view.php
+++ frontend/widgets/views/_filter_view.php
@@ -34,7 +34,7 @@ use yii\helpers\Url;
                     ?>
                     <div class="sidebar_checks" data-checked="<?= $checked ? ' true' : 'false'?>">
                         <input <?= isset($option['count']) && $option['count'] == 0 ? 'disabled' : ''  ?>  id="<?= $option['tax_option_id']?>"  class="custom-check" type="checkbox" <?= $checked ? ' checked' : ''?>  value="1">
-                        <label for="<?= $option['tax_option_id']?>"><span class="features-option"></span>
+                        <label class="features-option" for="<?= $option['tax_option_id']?>">
                             <a  class="filter-link <?= isset($option['count']) && $option['count'] == 0 ? 'disabled-link' : ''  ?>"  href="<?= $option_url?>">
                                 <?= $option['value']?>
                             </a>
