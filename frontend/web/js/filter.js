4819fb65abe13271dab9c3e195f613a47853f7ba
Vitaliy <dockdep@gmail.com>
2017-09-14 00:35:59 +0300

    start

--- frontend/web/js/filter.js
+++ frontend/web/js/filter.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 
     $('body').on('click', '.features-option', function(){
-        var link = $(this).siblings('a');
+        var link = $(this).closest('a');
         var url = link.attr('href');
         console.log(url);
         $.pjax({url: url, container: '#list-container',timeout:5000,scrollTo:false })
