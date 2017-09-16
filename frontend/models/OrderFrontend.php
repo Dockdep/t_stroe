4f1d18164f23874cb96524a02a7efda6c3e81a89
Vitaliy <dockdep@gmail.com>
2017-09-17 02:21:04 +0300

    work

--- frontend/models/OrderFrontend.php
+++ frontend/models/OrderFrontend.php
@@ -183,7 +183,6 @@
                     [
                         'name',
                         'phone',
-                        'confirm',
                     ],
                     'required',
                 ],
