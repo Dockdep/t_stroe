47eb2c25954dbb6770776d5932a624061d54ae71
Vitaliy <dockdep@gmail.com>
2017-09-17 01:06:22 +0300

    work

--- frontend/views/order/_form.php
+++ frontend/views/order/_form.php
@@ -1,278 +1,115 @@
-<?php
-    /**
-     * @var View          $this
-     * @var OrderFrontend $order
-     * @var Delivery[]    $deliveries
-     */
-    use artweb\artbox\ecommerce\models\Delivery;
-    use artweb\artbox\ecommerce\models\OrderPayment;
-    use common\components\CreditHelper;
-    use frontend\models\OrderFrontend;
-    use yii\bootstrap\ActiveForm;
-    use yii\bootstrap\Html;
-    use yii\helpers\ArrayHelper;
-    use yii\web\View;
+<div class="forms_pages form-new-customer">
+    <form  action="" method="post">
 
-?>
-
-<div class="forms_ form-register">
-    <?php
-        $form = ActiveForm::begin(
-            [
-                'id' => 'basket-form',
-            ]
-        );
-    ?>
-    <?php
-        if (\Yii::$app->user->isGuest) {
-            ?>
-            <div class="row">
-                <?php
-                    echo $form->field(
-                        $order,
-                        'name',
-                        [
-                            'options' => [
-                                'class' => 'col-xs-12 col-sm-6 col-md-6 input-wr medium-label',
-                            ],
-                        ]
-                    )
-                              ->textInput();
-                ?>
-            </div>
-            <?php
-        }
-    ?>
-
-    <div class="row">
-        <?php
-            echo $form->field(
-                $order,
-                'email',
-                [
-                    'errorOptions' => [
-                        'encode' => false,
-                    ],
-                    'options'      => [
-                        'class' => 'col-xs-12 col-sm-5 col-md-3 input-wr medium-label',
-                    ],
-                ]
-            )
-                      ->textInput();
-        ?>
-    </div>
-    <?php
-        if (\Yii::$app->user->isGuest) {
-            ?>
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 input-wr emails_txt">
-                    <?= \Yii::t('app', 'ordform1') ?><br/>
-                    <?= \Yii::t('app', 'ordform2') ?>
-                    <a class="terms_of_use" href="#"><?= \Yii::t('app', 'ordform3') ?></a>
+        <div class="row">
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr">
+                <div class="form-group required">
+                    <label class="control-label" for="">Имя</label>
+                    <input type="text" id="">
                 </div>
             </div>
-            <?php /*
+
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr">
+                <div class="form-group required">
+                    <label class="control-label" for="">Фамилия</label>
+                    <input type="text" id="">
+                </div>
+            </div>
+
+        </div>
+
         <div class="row">
-            <?php
-                echo $form->field(
-                    $order,
-                    'subscribe',
-                    [
-                        'options'  => [
-                            'class' => 'col-xs-12 col-sm-12 input-wr check-box-form',
-                        ],
-                        'template' => "{input}\n{label}\n{hint}\n{error}",
-                    ]
-                )
-                          ->checkbox([ 'class' => 'custom-check' ], false)
-                          ->label(
-                              Html::tag('span') . Html::a(
-                                  $order->getAttributeLabel('subscribe'),
-                                  [
-                                      '',
-                                      '#' => '',
-                                  ]
-                              )
-                          );
-                echo $form->field(
-                    $order,
-                    'notRegister',
-                    [
-                        'options'  => [
-                            'class' => 'col-xs-12 col-sm-12 input-wr check-box-form',
-                        ],
-                        'template' => "{input}\n{label}\n{hint}\n{error}",
-                    ]
-                )
-                          ->checkbox([ 'class' => 'custom-check' ], false)
-                          ->label(
-                              Html::tag('span') . Html::a(
-                                  $order->getAttributeLabel('notRegister'),
-                                  [
-                                      '',
-                                      '#' => '',
-                                  ]
-                              )
-                          );
-            ?>
-        </div> */
-            ?>
-            <?php
-        }
-    ?>
-    <div class="row">
-        <?php
-            echo $form->field(
-                $order,
-                'phone',
-                [
-                    'options' => [
-                        'class' => 'col-xs-12 col-sm-5 col-md-3 input-wr medium-label',
-                    ],
-                ]
-            )
-                      ->textInput(
-                          [
-                              "placeholder" => "+38(_ _ _) _ _ _- _ _ - _ _",
-                          ]
-                      );
-        ?>
-    </div>
-    <div class="row">
-        <?php
-            echo $form->field(
-                $order,
-                'city',
-                [
-                    'options' => [
-                        'class' => 'col-xs-12 col-sm-5 col-md-3 input-wr medium-label',
-                    ],
-                ]
-            )
-                      ->textInput();
-            echo $form->field(
-                $order,
-                'adress',
-                [
-                    'options' => [
-                        'class' => 'col-xs-12 col-sm-5 col-md-3 input-wr medium-label',
-                    ],
-                ]
-            )
-                      ->textInput();
-        ?>
-    </div>
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr">
+                <div class="form-group field-signupform-email required">
+                    <label class="control-label" for="signupform-email">Ваш e-mail</label>
+                    <input type="text" id="signupform-email" class="form-control" name="SignupForm[email]">
 
-    <div class="row delivery-wrapper">
-        <div class="col-xs-12 col-sm-12 input-wr required">
-            <label for="">Вариант доставки</label>
+
+                </div>                        </div>
+
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr  phones_mask">
+                <div class="form-group ">
+                    <label>Ваш мобильный телефон</label>
+                    <input type="text" id="signupform-phone" class="form-control" name="SignupForm[phone]" placeholder="+38(_ _ _) _ _ _- _ _ - _ _">
+                </div>
+            </div>
         </div>
 
-        <div class="col-xs-12">
-            <div class="style radio_custom">
-                <input type="radio" id="radio-1-1" name="group-1">
-                <label for="radio-1-1">Доставка по Киеву и области</label>
 
+
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr ">
+                <div class="form-group">
+                    <label>Город</label>
+                    <input type="text">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-5 col-md-3 input-wr ">
+                <div class="form-group">
+                    <label >Ваш адрес</label>
+                    <input type="text">
+                </div>
+            </div>
+        </div>
+
+        <div class="row delivery-wrapper">
+            <div class="col-xs-12 col-sm-12 input-wr required">
+                <label for="">Вариант доставки</label>
             </div>
 
-            <div class="style radio_custom">
-                <input type="radio" id="radio-1-2"  name="group-1">
-                <label for="radio-1-2">Доставка по Украине</label>
+            <div class="col-xs-12">
+                <div class="style radio_custom">
+                    <input type="radio" id="radio-1-1" name="group-1">
+                    <label for="radio-1-1">Доставка по Киеву и области</label>
+
+                </div>
+
+                <div class="style radio_custom">
+                    <input type="radio" id="radio-1-2"  name="group-1">
+                    <label for="radio-1-2">Доставка по Украине</label>
+                </div>
+
+                <div class="style radio_custom">
+                    <input type="radio" id="radio-1-3"  name="group-1">
+                    <label for="radio-1-3">Самовывоз</label>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row payment-wrapper">
+            <div class="col-xs-12 col-sm-12 input-wr required">
+                <label for="">Способы оплаты</label>
             </div>
 
-            <div class="style radio_custom">
-                <input type="radio" id="radio-1-3"  name="group-1">
-                <label for="radio-1-3">Самовывоз</label>
+            <div class="col-xs-12">
+                <div class="style radio_custom">
+                    <input type="radio" id="radio-2-1" name="group-2">
+                    <label for="radio-2-1">Оплата наличными</label>
+                </div>
+
+                <div class="style radio_custom">
+                    <input type="radio" id="radio-2-2"  name="group-2">
+                    <label for="radio-2-2">Оплата по безналичному расчету</label>
+                </div>
+
             </div>
-
-        </div>
-    </div>
-
-    <div class="row payment-wrapper">
-        <div class="col-xs-12 col-sm-12 input-wr required">
-            <label for="">Способы оплаты</label>
         </div>
 
-        <div class="col-xs-12">
-            <div class="style radio_custom">
-                <input type="radio" id="radio-2-1" name="group-2">
-                <label for="radio-2-1">Оплата наличными</label>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 input-wr">
+                <div class="form-group ">
+                    <label>Комментарий</label>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
             </div>
+        </div>
 
-            <div class="style radio_custom">
-                <input type="radio" id="radio-2-2"  name="group-2">
-                <label for="radio-2-2">Оплата по безналичному расчету</label>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 button-wr button-left">
+                <button type="submit">ОФОРМИТЬ ЗАКАЗ</button>
             </div>
-
         </div>
-    </div>
-
-    <div class="row">
-        <?php
-            echo $form->field(
-                $order,
-                'comment',
-                [
-                    'options' => [
-                        'class' => 'col-xs-12 col-sm-10 col-md-6 input-wr medium-label _area_',
-                    ],
-                ]
-            )
-                      ->textarea(
-                          [
-                              'cols' => 30,
-                              'rows' => 10,
-                          ]
-                      );
-        ?>
-        <div class="col-xs-12 col-sm-12 add_inform"><?= \Yii::t('app', 'dopinfo') ?></div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 input-wr check-box-form check-box-black plus_txt">
-            <?php
-                echo $form->field(
-                    $order,
-                    'confirm',
-                    [
-                        'options'  => [
-                            'class' => 'form-group',
-                        ],
-                        'template' => "{input}\n{label}\n{hint}\n{error}",
-                    ]
-                )
-                          ->checkbox(
-                              [
-                                  'class' => 'custom-check',
-                              ],
-                              false
-                          )
-                          ->label(
-                              Html::tag('span') . Html::a(
-                                  $order->getAttributeLabel('confirm'),
-                                  [
-                                      '',
-                                      '#' => '',
-                                  ]
-                              )
-                          );
-            ?>
-            <a class="rules_of" href="#"><?= \Yii::t('app', 'rules1') ?></a>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 input-wr">
-            <button type="submit"><?= \Yii::t('app', 'makeorder') ?></button>
-        </div>
-    </div>
-    <?php
-        $form::end();
-    ?>
+    </form>
 </div>
-
-<?php
-    $js = "
-
-$('#orderfrontend-phone').mask('+38(000)000-00-00');";
-    $this->registerJs($js, View::POS_READY);
-
-?>
--- frontend/views/order/_form_guest.php
+++ frontend/views/order/_form_guest.php
@@ -13,22 +13,26 @@
     use yii\widgets\ActiveForm;
 
 ?>
-<div class="style description_list-wrapper hidden-xs">
-    <ul class="description_list">
-        <li class="active">
-            <a class="desk_name" href="#"><?= \Yii::t('app','newbuyer')?></a>
-        </li>
-        
-        <li>
-            <a class="desk_name" href="#"><?= \Yii::t('app','registed')?></a>
-        </li>
-    
-    </ul>
-</div>
-<div class="style desk_blocks-wr">
-    <div class="active desk_list-wr active-mobile">
-        <a class="btn_mobil_show_desk style hidden-sm hidden-md hidden-lg" href="#"><?= \Yii::t('app','newbuyer')?></a>
-        <?php
+<div class="col-xs-12 col-sm-12">
+    <div class="style basket-tabs_list-wr hidden-xs">
+        <ul>
+            <li class="active">
+                <a href="#">Новый покупатель</a>
+            </li>
+
+            <li>
+                <a href="#">Я уже зарегестрирован</a>
+            </li>
+        </ul>
+    </div>
+
+    <!------------------------>
+    <div class="style tabs_basket-blocks">
+        <div class="style tabs_basket-mobile active-mobile">
+            <a class="btn_mobil_show_desk style hidden-sm hidden-md hidden-lg" href="#">Новый покупатель</a>
+            <div class="style hidden_tabs">
+
+            <?php
             echo $this->render(
                 '_form',
                 [
@@ -36,70 +40,63 @@
                 ]
             );
         ?>
-    </div>
-    <div class="desk_list-wr">
-        <a class="btn_mobil_show_desk style hidden-sm hidden-md hidden-lg" href="#"><?= \Yii::t('app','registed')?></a>
-        <div class="style desk_delivery">
-            <div class="forms_ already-registered">
+
+            </div>
+        </div>
+
+        <div class="style tabs_basket-mobile">
+            <a class="btn_mobil_show_desk style hidden-sm hidden-md hidden-lg" href="#">Я уже зарегестрирован</a>
+            <div class="style hidden_tabs">
                 <div class="row">
-                    <div class="col-xs-12 col-sm-5 col-md-3">
-                        <?php $form = ActiveForm::begin([
-                            "action" =>  Url::to(['site/login', 'referer' => Url::current([], true)]),
-                        ]); ?>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 input-wr medium-label">
-                                <?= $form->field(new LoginForm(), 'email')->textInput(['autofocus' => true]) ?>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12 input-wr medium-label">
-                                <?= $form->field(new LoginForm(), 'password')->passwordInput() ?>
-                            </div>
-                        </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4">
+                        <form action="#">
+                            <form id="w2" action="" method="post">
 
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 input-wr check-box-form">
-                                <?= $form->field(new LoginForm(), 'rememberMe', [ 'template' => "{input}\n{label}\n{error}" ])
-                                    ->label('<span></span>'.\Yii::t('app','rememberme'))
-                                    ->checkbox([
-                                        'class'    => 'custom-check',
-                                        'checked' => 'checked',
-                                    ], false) ?>
-                            </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 input-wr medium-label">
+                                        <div class="form-group required">
+                                            <label class="control-label">Email</label>
+                                            <input type="text">
+                                        </div>
+                                    </div>
+                                </div>
 
-                        </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 col-md-12 input-wr medium-label">
+                                        <div class="form-group required">
+                                            <label class="control-label">Пароль</label>
+                                            <input type="password">
+                                        </div>
+                                    </div>
+                                </div>
 
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 input-wr">
-                                <?= Html::submitButton(\Yii::t('app','enter')) ?>
-                            </div>
-                        </div>
-                        <?php
-                        ActiveForm::end();
-                        ?>
-                    </div>
-                    <div class="col-xs-12 col-sm-5 col-md-3">
-                        <div class="btns_reg">
-                            <?= Html::a(\Yii::t('app','restore'), [ 'site/request-password-reset' ],
-                                [
-                                    'class'=>'restore_password_'
-                                ]) ?>
-                            <span class="no_account_yet"><?= \Yii::t('app', 'noaccs') ?></span>
-                            <?= Html::a(\Yii::t('app','reg'), [ 'site/signup' ],[
-                                'class'=>'to-register-link'
-                            ]) ?>
-                        </div>
-                        <div class="btns_reg btns_social-login">
-                            <span class="no_account_yet"><?= \Yii::t('app', 'socbutreg') ?></span>
-                            <div class="style icon-soc-login-wr">
-                                <span class="social_login vk_login"></span>
-                                <span class="social_login fb_login"></span>
-                                <span class="social_login gpluse_login"></span>
-                            </div>
-                        </div>
+                                <div class="row">
+                                    <div class="col-xs-12 col-sm-12 input-wr sidebar_checks">
+                                        <div class="form-group field-loginform-rememberme">
+                                            <input type="checkbox" id="loginform-rememberme" name="LoginForm[rememberMe]" value="1" checked="">
+                                            <label class="control-label" for="loginform-rememberme"><a>Запомнить меня</a></label>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+
+                                    <div class="col-xs-12 col-sm-12 button-wr button-left">
+
+                                        <button type="submit">Войти</button>
+                                        <div class="btns_reg">
+                                            <a class="restore_password_" href="restore.html">Забыли пароль?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!------------------------>
+
 </div>
