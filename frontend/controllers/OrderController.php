8dc7e1a232bd72626eab5d533e1e9474e100f3d5
Vitaliy <dockdep@gmail.com>
2017-09-17 02:21:38 +0300

    work

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -47,10 +47,7 @@
                 $order->adress = $user->address;
             }
 
-            if (!empty( $models ) && $order->load(\Yii::$app->request->post())) {
-                $order->validate();
-                print_r($order->getErrors());
-                die();
+            if (!empty( $models ) && $order->load(\Yii::$app->request->post()) && $order->validate()) {
                 if (\Yii::$app->user->isGuest && !empty( $order->email )) {
                     $password = \Yii::$app->security->generateRandomString(6);
                     $signup = new SignupForm(
