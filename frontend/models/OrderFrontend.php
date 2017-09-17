dc84babfdeffe934d89e933cfe3d03bba58a9666
Vitaliy <dockdep@gmail.com>
2017-09-18 01:19:26 +0300

    work

--- frontend/models/OrderFrontend.php
+++ frontend/models/OrderFrontend.php
@@ -322,7 +322,7 @@
 
         public function sync()
         {
-            $data = ArrayHelper::toArray(Order::find()->where(['id'=> $this->id])->joinWith('products')->all());
+            $data = ArrayHelper::toArray(Order::find()->where(['order.id'=> $this->id])->joinWith('products')->all());
             $data = json_encode($data);
             print_r($data);
             die();
