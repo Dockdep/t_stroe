fc912c35d1582df33c62124510bdab716a48333f
Vitaliy <dockdep@gmail.com>
2017-09-18 01:43:16 +0300

    work

--- frontend/models/OrderFrontend.php
+++ frontend/models/OrderFrontend.php
@@ -327,7 +327,7 @@
              */
             $data = Order::find()->where(['order.id'=> $this->id])->joinWith('products')->one();
             $array = ArrayHelper::toArray($data);
-            $array['items'] = $data->products;
+            $array['items'] = ArrayHelper::toArray($data->products);
 //            print_r($data->products);
             print_r($array);
             die();
