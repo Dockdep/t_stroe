d56a52d7f4f0b68deda41ca061a5f8b3e67c5e32
Vitaliy <dockdep@gmail.com>
2017-09-19 02:13:53 +0300

    work

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -2,19 +2,46 @@
     
     namespace frontend\controllers;
 
+    use artweb\artbox\ecommerce\models\Brand;
+    use artweb\artbox\ecommerce\models\BrandLang;
+    use artweb\artbox\ecommerce\models\Product;
     use artweb\artbox\language\models\Language;
     use frontend\models\SearchForm;
     use yii\helpers\Url;
     use yii\data\ArrayDataProvider;
     use yii\data\Pagination;
+    use yii\web\HttpException;
 
     class SearchController extends \yii\web\Controller
     {
         public function actionMain($word,$action)
         {
-            print_r($word);
-            print_r($action);
-            die();
+            $data=[];
+            switch ($action){
+                case 0:
+                    print_r("по номеру товара");
+                    break;
+                case 1:
+                    $data = Product::find()
+                        ->joinWith(['lang','product_variant','product_variant.lang','brand','brand.lang'])
+                        ->andWhere(
+                            [
+                                'ilike',
+                                BrandLang::tableName() . '.title',
+                                $word,
+                            ]
+                        )->all();
+                    break;
+                case 2:
+                    print_r("по названию товара");
+                    break;
+                default:
+                    throw new HttpException(404, 'Page not found');
+                    break;
+            }
+            return $this->render('search', [
+                'data' => $data
+            ]);
         }
 
     }
