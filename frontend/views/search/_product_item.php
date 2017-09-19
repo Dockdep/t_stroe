96ef5736723697c97133b95c48d49b8812f1ba35
dockdep <dockdep@gmail.com>
2017-09-19 22:13:10 +0300

    work

--- frontend/views/search/_product_item.php
+++ frontend/views/search/_product_item.php
@@ -1,5 +1,26 @@
 <?php
+/**
+ * @var $model \artweb\artbox\ecommerce\models\Product
+ */
 use artweb\artbox\components\artboximage\ArtboxImageHelper;
+use common\models\CustomerCategoryDiscount;
+use yii\helpers\Html;
+
+if(!\Yii::$app->user->isGuest){
+    $discountCategory = CustomerCategoryDiscount::find()->where(['category_id'=>$model->category->id,'customer_id'=>\Yii::$app->user->identity->id])->one();
+    if(!$discountCategory instanceof CustomerCategoryDiscount && isset($model->parentAR))
+    {
+        $discountCategory = CustomerCategoryDiscount::find()->where(['category_id'=>$model->parentAR->id,'customer_id'=>\Yii::$app->user->identity->id])->one();
+        if(!$discountCategory instanceof CustomerCategoryDiscount && isset($model->parentAR->parentAR)){
+            $discountCategory = CustomerCategoryDiscount::find()->where(['category_id'=>$model->parentAR->parentAR->id,'customer_id'=>\Yii::$app->user->identity->id])->one();
+        }
+
+    }
+} else {
+    $discountCategory = null;
+}
+$discountData = $model->discountPrice($discountCategory);
+
 ?>
 <tr>
     <td class="img-analogs">
@@ -22,10 +43,26 @@ use artweb\artbox\components\artboximage\ArtboxImageHelper;
         ?>
     </td>
     <td class="title-analog-td">
-        <p><?= $model->lang->title ?></p>
+        <?php
+        echo Html::a(
+            $model->lang->title,
+            [
+                'catalog/product',
+                'product' => $model->lang->alias,
+                'variant' => $model->enabledVariants[0]->sku,
+            ],
+            [
+                'data-pjax' => 0,
+                'title'     => $model->lang->title,
+            ]
+        );
+        ?>
         <span>Код: <?= $model->variant->lang->title ?></span>
     </td>
     <td><?= $model->variant->stock ?> шт.</td>
-    <td class="analog-price-td">2000 грн.</td>
-    <td><a class="analogs-buy modal-link" data-form="basket_modal" href="#" title="Купить"></a></td>
+    <td class="analog-price-td"><?= $discountData['discount'] ?> грн.</td>
+    <td>
+
+        <a class="analogs-buy modal-link" data-form="basket_modal" href="#" title="Купить"></a>
+    </td>
 </tr>
