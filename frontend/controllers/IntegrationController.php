8cf950bcc543ae378fa5c5a7e384965bba154af5
dockdep <dockdep@gmail.com>
2017-09-24 16:16:45 +0300

    work

--- frontend/controllers/IntegrationController.php
+++ frontend/controllers/IntegrationController.php
@@ -83,8 +83,8 @@ class IntegrationController extends Controller{
 
     public function actionImportOrders(){
         try{
-            if($data = \Yii::$app->request->post("data")){
-                // $data = $this->getItemData();
+            //if($data = \Yii::$app->request->post("data")){
+                $data = $this->getItemData();
                 $data = json_decode($data);
                 if(is_array($data)){
                     foreach ($data as $item){
@@ -94,9 +94,9 @@ class IntegrationController extends Controller{
                     throw new Exception("Данные о пользователях ожидаются в виде массива.");
                 }
                 die(\GuzzleHttp\json_encode($this->result));
-            }else {
-                throw new Exception("Отсутствует data");
-            }
+//            }else {
+//                throw new Exception("Отсутствует data");
+//            }
 
 
         } catch (Exception $e) {
