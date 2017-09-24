3b5cdfe8603098f212ca4654ff2e74c8f9446fbd
dockdep <dockdep@gmail.com>
2017-09-24 19:59:37 +0300

    work

--- frontend/views/order/_form_guest.php
+++ frontend/views/order/_form_guest.php
@@ -52,57 +52,49 @@
                         <?php $form = ActiveForm::begin([
                             "action" =>  Url::to(['site/login'])
                         ]); ?>
-
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 input-wr medium-label">
-                                <div class="form-group field-loginform-email required">
-                                    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-                                </div>
+                                <?= $form->field(new LoginForm(), 'email')->textInput(['autofocus' => true]) ?>
                             </div>
                         </div>
-
                         <div class="row">
                             <div class="col-xs-12 col-sm-12 col-md-12 input-wr medium-label">
-                                <div class="form-group field-loginform-password required">
-                                    <?= $form->field($model, 'password')->passwordInput() ?>
-                                </div>
+                                <?= $form->field(new LoginForm(), 'password')->passwordInput() ?>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class="col-xs-12 col-sm-12 input-wr sidebar_checks">
-                                <div class="form-group field-loginform-rememberme">
-                                    <?= $form->field($model, 'rememberMe', [ 'template' => "{input}\n{label}\n{error}" ])
-                                        ->label('<a>Запомнить меня</a>')
-                                        ->checkbox([
-                                            'class'    => 'custom-check',
-                                            'checked' => 'checked',
-                                        ],false) ?>
-                                </div>
+                            <div class="col-xs-12 col-sm-12 input-wr check-box-form">
+                                <?= $form->field(new LoginForm(), 'rememberMe', [ 'template' => "{input}\n{label}\n{error}" ])
+                                    ->label('<span></span>Запомнить меня')
+                                    ->checkbox([
+                                        'class'    => 'custom-check',
+                                        'checked' => 'checked',
+                                    ], false) ?>
                             </div>
 
                         </div>
 
                         <div class="row">
-                            <div class="col-xs-12 col-sm-12 button-wr button-left" >
-                                <button type="submit">Войти</button>
-                                <div class="btns_reg modal_login_reg_btns">
-
-                                    <?=
-
-                                    Html::a('Забыли пароль?', [ 'site/request-password-reset' ],
-                                        [
-                                            'class'=>'restore_password_'
-                                        ]) ?>
-                                    <?= Html::a('Зарегистрироваться', [ 'site/signup' ],[
-                                        'class'=>'to-register-link'
-                                    ]) ?>
-                                </div>
+                            <div class="col-xs-12 col-sm-12 input-wr">
+                                <?= Html::submitButton('Войти') ?>
                             </div>
                         </div>
                         <?php
                         ActiveForm::end();
                         ?>
+                    </div>
+                    <div class="col-xs-12 col-sm-5 col-md-3">
+                        <div class="btns_reg">
+                            <?= Html::a('Восстановить пароль', [ 'site/request-password-reset' ],
+                                [
+                                    'class'=>'restore_password_'
+                                ]) ?>
+                            <span class="no_account_yet">Нет учетной записи?</span>
+                            <?= Html::a('Зарегистрироваться', [ 'site/signup' ],[
+                                'class'=>'to-register-link'
+                            ]) ?>
+                        </div>
                     </div>
                 </div>
             </div>
