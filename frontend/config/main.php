5235f9cfc5c4c916c0a30360c43b4e51ff173c6a
dockdep <dockdep@gmail.com>
2017-09-25 03:24:19 +0300

    work

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -80,6 +80,7 @@
                 'showScriptName'  => false,
                 'class'           => 'artweb\artbox\language\components\LanguageUrlManager',
                 'rules'           => [
+                    'product/<product:[\w-]+>'                        => 'catalog/product',
                     'product/<product:[\w-]+>/<variant>'                        => 'catalog/product',
                     [
                         'class'     => 'artweb\artbox\ecommerce\CatalogUrlManager',
