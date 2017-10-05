77f2476931a6d9b7f0afb68a422c2d27afe4b304
Vitaliy <dockdep@gmail.com>
2017-10-06 02:56:05 +0300

    work

--- frontend/views/search/stock_product_item.php
+++ frontend/views/search/stock_product_item.php
@@ -7,7 +7,12 @@
         <span>Код: <?= $model->KOD_TOVARA ?></span>
     </td>
     <td><?= $model->amount ?> шт.</td>
+    <?php if($model->price > 0) {?>
     <td class="analog-price-td"><?= round(((100-25)/100) * $model->price,2); ?></td>
+    <?php }else {?>
+    <td class="analog-price-td">цену и наличие уточняйте у менеджера</td>
+    <?php } ?>
+
     <td>
 
         <a class="analogs-buy modal-link btn_buy_analog_cat" data-discount="25" data-price="<?= round( $model->price,2); ?>" data-discount_price="<?= round(((100-25)/100) * $model->price,2); ?>" data-name="<?= $model->GADES; ?>"  data-productcode="<?= $model->KOD_TOVARA; ?>" data-priceid="<?= $model->ID_Prices; ?>"  data-variant="<?= !empty($model->KOD_TOVARA) ?$model->KOD_TOVARA: $model->ID_Prices?>"  data-form="basket_modal" title="Купить"></a>
