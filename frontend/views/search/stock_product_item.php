f868ca2bfe9cbc4191c2e501c21b86455ab3c286
Vitaliy <dockdep@gmail.com>
2017-10-06 02:59:55 +0300

    work

--- frontend/views/search/stock_product_item.php
+++ frontend/views/search/stock_product_item.php
@@ -14,7 +14,7 @@
     <?php } ?>
 
     <td>
-        <?php if($discountData['price']> 0) {?>
+        <?php if($model->price > 0) {?>
             <a class="analogs-buy modal-link btn_buy_analog_cat" data-discount="25" data-price="<?= round( $model->price,2); ?>" data-discount_price="<?= round(((100-25)/100) * $model->price,2); ?>" data-name="<?= $model->GADES; ?>"  data-productcode="<?= $model->KOD_TOVARA; ?>" data-priceid="<?= $model->ID_Prices; ?>"  data-variant="<?= !empty($model->KOD_TOVARA) ?$model->KOD_TOVARA: $model->ID_Prices?>"  data-form="basket_modal" title="Купить"></a>
         <?php }else {?>
             <a class="analogs-buy" title="Купить"></a>
