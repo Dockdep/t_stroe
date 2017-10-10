56ed50b8ced6e5fc62307bc642f01c83bb5d86b9
dockdep <dockdep@gmail.com>
2017-10-11 01:45:12 +0300

    work

--- frontend/views/basket/basket_table.php
+++ frontend/views/basket/basket_table.php
@@ -126,7 +126,7 @@ $basket_sum = 0;
         <?php
         foreach ($analogs as $itemId => $itemData) {
             ?>
-            <tr class="variant_tr" data-variant="<?= $itemId ?>">
+            <tr class="variant_tr" data-variant="<?= $itemData['KOD_TOVARA'] ?>">
                 <td>
                     <img src="/images/no-img-80.png" alt="">
                 </td>
