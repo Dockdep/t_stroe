5ab032b9b328755a5fd085a4280b7452d44dbb86
Vitaliy <dockdep@gmail.com>
2017-10-12 21:18:03 +0300

    work

--- frontend/widgets/MenuWidget.php
+++ frontend/widgets/MenuWidget.php
@@ -29,7 +29,7 @@
                 ->orderBy('category_lang.title')
                 ->indexBy('id')
                 ->all();
-            print_r($categories);
+            print_r(\GuzzleHttp\json_encode($categories));
             die();
             switch ($this->type){
                 case 'full':
