dbea82f68a40b0b0d39447e3ae85d490d2cb742f
icemate <icemate.job@gmail.com>
2017-10-13 16:05:48 +0300

    3

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,14 +16,13 @@ class AppAsset extends AssetBundle
         "/css/style.css"
     ];
     public $js = [
+        'http://maps.google.com/maps/api/js?key=AIzaSyCgyPQhOmGEwyAdZ0uS6fJLLvnNxgncfy0',
+        'js/markerclusterer.js',
         '/js/script.js',
         '/js/artbox_basket.js',
         '/js/owl.carousel.min.js',
         '/js/jquery.mask.min.js',
         '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.js',
-        'http://maps.google.com/maps/api/js?key=AIzaSyCgyPQhOmGEwyAdZ0uS6fJLLvnNxgncfy0',
-        'js/markerclusterer.js',
-
     ];
 
     public $depends = [
