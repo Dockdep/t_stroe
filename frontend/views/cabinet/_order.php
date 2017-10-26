0c88cf8d53b5349381ae5e1f0f2cc4d0da8da29e
Vitaliy <dockdep@gmail.com>
2017-10-27 01:28:32 +0300

    work

--- frontend/views/cabinet/_order.php
+++ frontend/views/cabinet/_order.php
@@ -22,7 +22,6 @@
 
         <table cellpadding="0" cellspacing="0" border="0" class="tb-cab-history">
             <?php foreach ($model->products as $product) { ?>
-                <?php if (isset($product->name) && !empty($product->name)){?>
                     <tr>
                         <td class="cab-history-img">
                         </td>
@@ -34,20 +33,6 @@
                         <td class="cab-history-num"><?= $product->count ?> шт.</td>
                         <td class="cab-history-price"><?= round(($product->count * $product->discount_price),2) ?> грн.</td>
                     </tr>
-                    <?php } else {?>
-                    <tr>
-                        <td class="cab-history-img">
-
-                        </td>
-                        <td class="cab-history-name">
-                            <!--название выводить или в "a" или в "<p></p>" если нету ссылки-->
-                            <a href="#"><?= $product->remote_id ?></a>
-                            <span><?= round($product->discount_price,2) ?> грн.</span>
-                        </td>
-                        <td class="cab-history-num"><?= $product->count ?> шт.</td>
-                        <td class="cab-history-price"><?= round(($product->count * $product->discount_price),2) ?> грн.</td>
-                    </tr>
-                    <?php } ?>
             <?php } ?>
         </table>
 
