e0939e8efb79109e2a88bc624bf852d8e8d7f0fe
dockdep <dockdep@gmail.com>
2018-05-21 23:56:24 +0300

    work

--- frontend/views/catalog/card.php
+++ frontend/views/catalog/card.php
@@ -196,7 +196,7 @@ $PriceData = $product->discountPrice($discountCategory);
                     </div>
 
                     <div style="padding: 15px;">
-                        <p>Аналог для:</p>
+                        <h2>Аналог для:</h2>
                         <ul>
                             <?php foreach ($sproducts as $sproduct){?>
                             <li><?= $sproduct ?></li>
